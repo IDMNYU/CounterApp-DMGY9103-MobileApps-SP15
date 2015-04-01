901b106ebba0ac476bec3fa22e4bb6ab8d5d01ae
Chris Ricca <christopher.ricca@gmail.com>
2015-04-01 17:37:34 -0400

    removed storyboards

--- counter/AppDelegate.m
+++ counter/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CounterViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    UIViewController* initialViewController = [[CounterViewController alloc] initWithNibName:@"CounterViewController" bundle:nil];
+    
+    self.window.rootViewController = initialViewController;
+
+    [self.window makeKeyAndVisible];
+
+    
     return YES;
 }
 
